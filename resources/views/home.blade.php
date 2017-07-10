a392e3378ffe56eddb199b9e1b47300005a3cc13
WILFRIDORAMIREZ <wilirm23@gmail.com>
2017-07-10 15:52:17 -0500

    se modifico el home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,16 @@
 @section('htmlheader_title')
 	Home
 @endsection
-
+@section('contentheader_title')
+Panel de Control
+@endsection
 
 @section('main-content')
 	<div class="container spark-screen">
 		<div class="row">
 			<div class="col-md-10 col-md-offset-1">
 				<div class="panel panel-default">
-					<div class="panel-heading">Home</div>
+					<div class="panel-heading">Bienvenido</div>
 
 					<div class="panel-body">
 						{{ trans('adminlte_lang::message.logged') }}

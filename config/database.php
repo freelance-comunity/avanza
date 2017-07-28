6aec276086665fef08c061b257b1544dcdd01af4
jcmontejo <jncrlsmontejo@gmail.com>
2017-07-28 11:48:31 -0500

    Se modifico gestor de BD por pgsql

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -46,38 +46,39 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
+    'sqlite' => [
+    'driver' => 'sqlite',
+    'database' => env('DB_DATABASE', database_path('database.sqlite')),
+    'prefix' => '',
+    ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
-            'engine' => null,
-        ],
+    'mysql' => [
+    'driver' => 'mysql',
+    'host' => env('DB_HOST', 'localhost'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => null,
+    ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-        ],
+    'pgsql' => [
+    'driver' => 'pgsql',
+    'host' => env('DB_HOST', 'ec2-50-19-89-124.compute-1.amazonaws.com
+        '),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'delhl48b16ivip'),
+    'username' => env('DB_USERNAME', 'vkinjqhorpufsj'),
+    'password' => env('DB_PASSWORD', '17da9d09e14664d67d254584e13f2bb988813c53c95f7cc6bdade10d93f878c2'),
+    'charset' => 'utf8',
+    'prefix' => '',
+    'schema' => 'public',
+    ],
 
     ],
 
@@ -107,15 +108,15 @@ return [
 
     'redis' => [
 
-        'cluster' => false,
+    'cluster' => false,
 
-        'default' => [
-            'host' => env('REDIS_HOST', 'localhost'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
-        ],
+    'default' => [
+    'host' => env('REDIS_HOST', 'localhost'),
+    'password' => env('REDIS_PASSWORD', null),
+    'port' => env('REDIS_PORT', 6379),
+    'database' => 0,
+    ],
 
     ],
 
-];
+    ];

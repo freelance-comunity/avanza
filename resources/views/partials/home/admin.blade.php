ee25cb210087f7214ccaf904b9261875b1468e13
WILFRIDORAMIREZ <wilirm23@gmail.com>
2017-11-09 10:21:18 -0600

    no se muestra la sesion del Web master

--- resources/views/partials/home/admin.blade.php
+++ resources/views/partials/home/admin.blade.php
@@ -28,112 +28,112 @@ $closes = App\Models\Close::orderBy('created_at', 'desc')->take(3)->get();
 } 
 </style> --}}
 <div class="row">
-<div class="col-lg-3 col-md-4">
-  <!-- small box -->
-  <div class="small-box bg-aqua">
-    <div class="inner">
-      <h3>${{ number_format($vault->sum('ammount'),2) }}</h3>
-      <p>Total Bovéda</p>
+  <div class="col-lg-3 col-md-4">
+    <!-- small box -->
+    <div class="small-box bg-aqua">
+      <div class="inner">
+        <h3>${{ number_format($vault->sum('ammount'),2) }}</h3>
+        <p>Total Bovéda</p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-bank"></i>
+      </div>
+      <a href="{{ url('/report-vault') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
     </div>
-    <div class="icon">
-      <i class="fa fa-bank"></i>
-    </div>
-    <a href="{{ url('/report-vault') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
   </div>
-</div>
-<!-- ./col -->
-<div class="col-lg-3 col-md-4">
-  <!-- small box -->
-  <div class="small-box bg-green">
-    <div class="inner">
-      <h3>{{ $clients->count() }}</h3>
+  <!-- ./col -->
+  <div class="col-lg-3 col-md-4">
+    <!-- small box -->
+    <div class="small-box bg-green">
+      <div class="inner">
+        <h3>{{ $clients->count() }}</h3>
 
-      <p>Total Clientes</p>
+        <p>Total Clientes</p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-users"></i>
+      </div>
+      <a href="{{ url('/report-clients') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
     </div>
-    <div class="icon">
-      <i class="fa fa-users"></i>
-    </div>
-    <a href="{{ url('/report-clients') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
   </div>
-</div>
 
-<!-- ./col -->
-<div class="col-lg-3 col-md-4">
-  <!-- small box -->
-  <div class="small-box bg-yellow">
-    <div class="inner">
-      <h3>{{ $credits->count() }}</h3>
+  <!-- ./col -->
+  <div class="col-lg-3 col-md-4">
+    <!-- small box -->
+    <div class="small-box bg-yellow">
+      <div class="inner">
+        <h3>{{ $credits->count() }}</h3>
 
-      <p>Total Créditos</p>
+        <p>Total Créditos</p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-paperclip"></i>
+      </div>
+      <a href="{{ url('/report-credits') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
     </div>
-    <div class="icon">
-      <i class="fa fa-paperclip"></i>
-    </div>
-    <a href="{{ url('/report-credits') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
   </div>
-</div>
-<!-- ./col -->
-<div class="col-lg-3 col-md-4">
-  <!-- small box -->
-  <div class="small-box bg-red">
-    <div class="inner">
-      <h3>${{ number_format($credits->sum('ammount'),2) }}</h3>
+  <!-- ./col -->
+  <div class="col-lg-3 col-md-4">
+    <!-- small box -->
+    <div class="small-box bg-red">
+      <div class="inner">
+        <h3>${{ number_format($credits->sum('ammount'),2) }}</h3>
 
-      <p>Total Ministrado</p>
+        <p>Total Ministrado</p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-money"></i>
+      </div>
+      <a href="{{ url('/report-credits') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
     </div>
-    <div class="icon">
-      <i class="fa fa-money"></i>
-    </div>
-    <a href="{{ url('/report-credits') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
   </div>
-</div>
-<!-- ./col -->
-<div class="col-lg-3 col-md-4">
-  <!-- small box -->
-  <div class="small-box bg-purple">
-    <div class="inner">
-      <h3>${{ number_format($expenditures->sum('ammount'),2) }}</h3>
+  <!-- ./col -->
+  <div class="col-lg-3 col-md-4">
+    <!-- small box -->
+    <div class="small-box bg-purple">
+      <div class="inner">
+        <h3>${{ number_format($expenditures->sum('ammount'),2) }}</h3>
 
-      <p>Total Gastos</p>
+        <p>Total Gastos</p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-shopping-cart"></i>
+      </div>
+      <a href="{{ url('/report-expenditures') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
     </div>
-    <div class="icon">
-      <i class="fa fa-shopping-cart"></i>
-    </div>
-    <a href="{{ url('/report-expenditures') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
   </div>
-</div>
-<!-- ./col -->
-<div class="col-lg-3 col-md-4">
-  <!-- small box -->
-  <div class="small-box bg-blue">
-    <div class="inner">
-      <h3>${{ number_format($payment,2) }}</h3>
+  <!-- ./col -->
+  <div class="col-lg-3 col-md-4">
+    <!-- small box -->
+    <div class="small-box bg-blue">
+      <div class="inner">
+        <h3>${{ number_format($payment,2) }}</h3>
 
-      <p>Proyección de Recuperación</p>
+        <p>Proyección de Recuperación</p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-dollar"></i>
+      </div>
+      <a href="{{ url('/report-payments') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
     </div>
-    <div class="icon">
-      <i class="fa fa-dollar"></i>
-    </div>
-    <a href="{{ url('/report-payments') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
   </div>
-</div>
-<!-- ./col -->
-<!-- ./col -->
-<div class="col-lg-3 col-md-4">
-  <!-- small box -->
-  <div class="small-box bg-orange">
-    <div class="inner">
-      <h3>${{ number_format($payments->sum('ammount'),2) }}</h3>
+  <!-- ./col -->
+  <!-- ./col -->
+  <div class="col-lg-3 col-md-4">
+    <!-- small box -->
+    <div class="small-box bg-orange">
+      <div class="inner">
+        <h3>${{ number_format($payments->sum('ammount'),2) }}</h3>
 
-      <p>Total Recuperado del día</p>
+        <p>Total Recuperado del día</p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-line-chart"></i>
+      </div>
+      <a href="{{ url('/report-payments') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
     </div>
-    <div class="icon">
-      <i class="fa fa-line-chart"></i>
-    </div>
-    <a href="{{ url('/report-payments') }}" class="small-box-footer">Descargar <i class="fa fa-file-pdf-o"></i></a>
   </div>
-</div>
-<!-- ./col -->
+  <!-- ./col -->
   {{-- <div class="col-lg-3 col-xs-6">
     <!-- small box -->
     <div class="small-box bg-navy">
@@ -185,11 +185,14 @@ $closes = App\Models\Close::orderBy('created_at', 'desc')->take(3)->get();
       <div class="box-body no-padding">
         <ul class="users-list clearfix">
           @foreach ($activities as $activity)
+          @if ($activity->user->id > 2)
           <li>
             <img src="{{asset('/uploads/avatars')}}/{{ $activity->user->avatar }}" alt="User Image" class="online">
             <a class="users-list-name" href="#">{{ $activity->user->name }}</a>
             {{-- <span class="users-list-date">{{ Carbon\Carbon::now() }}</span> --}}
           </li>
+          @endif
+          
           @endforeach
         </ul>
         <!-- /.users-list -->

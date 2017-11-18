68349d0d4cc17b5889ab5456138f0fd9f9d3c9f1
WILFRIDORAMIREZ <wilirm23@gmail.com>
2017-11-18 12:23:30 -0600

    moratorio

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -900,53 +900,7 @@ Route::get('najera', function () {
 
 Route::get('reportPayment', 'GeneralController@reportPayment');
 
-Route::get('moratorium', function () {
-    $credit = App\Models\Credit::all();
-    $date_now = \Carbon\Carbon::now()->toDateString();
-    $hour_now = \Carbon\Carbon::now()->toTimeString();
-    foreach ($credit as $key => $credit) {
-        $debt = $credit->debt;
-        $payments = $debt->payments;
 
-        foreach ($payments as $key => $payment) {
-            if ($payment->date <= $date_now && $payment->status == 'Pendiente' && $hour_now >= '11:00:00') {
-                $payment = App\Models\Payment::find($payment->id);
-                $payment->status = 'Vencido';
-                $payment->moratorium = 20;
-                $payment->total = $payment->ammount + $payment->moratorium;
-                $payment->balance = $payment->balance + 20;
-                $payment->save();
-
-                $debt = $payment->debt;
-                $debt->ammount = $debt->ammount + 20;
-                $debt->save();
-            }
-            if ($payment->date <= $date_now && $payment->status == 'Parcial' && $hour_now >= '11:00:00') {
-                $payment = App\Models\Payment::find($payment->id);
-                $payment->status = 'Vencido';
-                $payment->moratorium = 20;
-                $payment->total = $payment->ammount + $payment->moratorium;
-                $payment->balance = $payment->balance + 20;
-                $payment->save();
-
-                $debt = $payment->debt;
-                $debt->ammount = $debt->ammount + 20;
-                $debt->save();
-            }
-            if ($payment->status == 'Vencido') {
-                $latePayments = new App\Models\LatePayments;
-                $latePayments->late_number = $payment->number;
-                $latePayments->late_ammount = $payment->total;
-                $latePayments->late_payment = $payment->payment;
-                $latePayments->status = "Atrasado";
-                $latePayments->payment_id = $payment->id;
-                $latePayments->debt_id    = $debt->id;
-                $latePayments->save();
-            }
-        }
-    }
-    echo "MORATORIO APLICADO CORRECTAMENTE";
-});
 
 Route::resource('clientReferences', 'ClientReferencesController');
 
@@ -987,33 +941,39 @@ Route::get('applyMoratorium',function(){
                 $debt = $payment->debt;
                 $debt->ammount = $debt->ammount + 20;
                 $debt->save();
+            }             
+            $latePayments = $payment->latePayments;
+           
+              if ($payment->status == "Vencido") {
+                if ($latePayments->count() == 0) {
+                    $latePayments = new App\Models\LatePayments;
+                    $latePayments->late_number = $payment->number;
+                    $latePayments->late_ammount = $payment->total;
+                    $latePayments->late_payment = $payment->payment;
+                    $latePayments->status = "Atrasado";
+                    $latePayments->payment_id = $payment->id;
+                    $latePayments->debt_id    = $debt->id;
+                    $latePayments->branch_id = $debt->branch_id;
+                    $latePayments->region_id = $debt->region_id;
+                    $latePayments->save();
+                }
+
             }
-            if ($payment->status == 'Vencido') {
-                $latePayments = new App\Models\LatePayments;
-                $latePayments->late_number = $payment->number;
-                $latePayments->late_ammount = $payment->total;
-                $latePayments->late_payment = $payment->payment;
-                $latePayments->status = "Atrasado";
-                $latePayments->payment_id = $payment->id;
-                $latePayments->debt_id    = $debt->id;
-                $latePayments->branch_id = $debt->branch_id;
-                $latePayments->region_id = $debt->region_id;
-                $latePayments->save();
-            }
-        }
+
     }
-    echo "MORATORIO APLICADO CORRECTAMENTE";
+}
+echo "MORATORIO APLICADO CORRECTAMENTE";
 });
 
 Route::get('productos', function () {
 
- $credit = App\Models\Credit::all();
- foreach ($credit as $key => $credit) {
+   $credit = App\Models\Credit::all();
+   foreach ($credit as $key => $credit) {
     if ($credit->periodicity == "SEMANAL") {
-     echo $credit->periodicity;
-     echo $credit->adviser;
-     echo "<br>";
- }
+       echo $credit->periodicity;
+       echo $credit->adviser;
+       echo "<br>";
+   }
 
 }
 });
@@ -1034,8 +994,8 @@ Route::get('changePayments', function(){
 
     $py->region_id = $branch->region_id;
     $py->save();
-  }
-  echo "Listo";
+}
+echo "Listo";
 });
 
 Route::get('changeDebts', function(){
@@ -1207,4 +1167,6 @@ Route::get('view-restructures/{id}', function($id)
 });
 
 Route::Post('consolidate', 'GeneralController@consolidate');
+
+
 

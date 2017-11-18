6a2990baa382eeebc893705fdf77bebb07cca26c
jcmontejo <jncrlsmontejo@gmail.com>
2017-11-18 10:07:26 -0600

    procesos de reestructura de créditos y metodo de cobranza capital, interes y mora.

--- resources/views/credits/payment_table_promoter.blade.php
+++ resources/views/credits/payment_table_promoter.blade.php
@@ -2,11 +2,12 @@
 		<thead class="bg-primary">
 			<th style="width: 15px;">No. Cuota</th>
 			<th>Fecha</th>
-			<th>Total</th>
-			<th>Monto</th>
-			<th>Mora</th>
+			<th>Cuota a Pagar</th>
 			<th>Pagado</th>
-			<th>Adeudo</th>				
+			<th>Saldo Total</th>
+			<th>Saldo Capital</th>
+			<th>Saldo Intereses</th>
+			<th>Saldo Moratorios</th>
 			<th>Estatus</th>
 		</thead>
 		<tbody>
@@ -16,63 +17,60 @@
 			<tr class="active">
 				<td>{{ $payment->number }}</td>
 				<td>{{$payment->date->format('d F Y')}}</td>
-				<td> 
-					@if ($payment->date == $date_now)
-					<button type="button" class="btn btn-success btn-lg" {{-- onclick="myFunction()"  data-toggle="modal" data-target="#payment_{{ $payment->id }}" --}}>$ {{ number_format($payment->total, 2) }}
-					</button>
-					@else
-					<button type="button" class="btn bg-yellow btn-lg" {{-- onclick="myFunction()" data-toggle="modal" data-target="#payment_{{ $payment->id }}" --}}>$ {{ number_format($payment->total, 2) }}
-					</button>
-					@endif				
-				</td>
-				<td>$ {{ number_format($payment->ammount, 2) }}</td>
-				<td>$ {{ number_format($payment->moratorium, 2) }}</td>
+				<td>${{number_format($payment->total,2)}}</td>
 				<td>${{ number_format($payment->payment, 2)}}</td>
-				<td>${{ number_format($payment->balance, 2) }}</td>
+				<td>$ {{ number_format($payment->balance, 2) }}</td>
+				<td>$ {{ number_format($payment->capital, 2) }}</td>
+				<td>$ {{ number_format($payment->interest, 2) }}</td>
+				<td>$ {{ number_format($payment->moratorium, 2) }}</td>
 				<td><p>{{$payment->status}}</p></td>
 			</tr>
 			@elseif($debt->credit->periodicity == "CREDISEMANA" && $payment->status == "Parcial")
 			<tr class="warning">
 				<td>{{ $payment->number }}</td>
 				<td>{{$payment->date->format('d F Y')}}</td>
-				<td> <button type="button" class="btn btn-primary btn-lg" {{-- onclick="myFunction()"  data-toggle="modal" data-target="#credisemana{{$client->id}}" --}} >$ {{ number_format($payment->total, 2) }}</button></td>
-				<td>$ {{ number_format($payment->ammount, 2) }}</td>
-				<td>$ {{ number_format($payment->moratorium, 2) }}</td>
+				<td>${{number_format($payment->total,2)}}</td>
 				<td>${{ number_format($payment->payment, 2)}}</td>
-				<td>${{ number_format($payment->balance, 2) }}</td>
+				<td>$ {{ number_format($payment->balance, 2) }}</td>
+				<td>$ {{ number_format($payment->capital, 2) }}</td>
+				<td>$ {{ number_format($payment->interest, 2) }}</td>
+				<td>$ {{ number_format($payment->moratorium, 2) }}</td>
 				<td><p>{{$payment->status}}</p></td>
 			</tr>
 			@elseif($payment->status == "Parcial")
 			<tr class="warning">
 				<td>{{ $payment->number }}</td>
 				<td>{{$payment->date->format('d F Y')}}</td>
-				<td> <button type="button" class="btn btn-primary btn-lg" {{-- onclick="myFunction()" data-toggle="modal" data-target="#payment_{{ $payment->id }}" --}}>$ {{ number_format($payment->total, 2) }}</button></td>
-				<td>$ {{ number_format($payment->ammount, 2) }}</td>
-				<td>$ {{ number_format($payment->moratorium, 2) }}</td>
+				<td>${{number_format($payment->total,2)}}</td>
 				<td>${{ number_format($payment->payment, 2)}}</td>
-				<td>${{ number_format($payment->balance, 2) }}</td>
+				<td>$ {{ number_format($payment->balance, 2) }}</td>
+				<td>$ {{ number_format($payment->capital, 2) }}</td>
+				<td>$ {{ number_format($payment->interest, 2) }}</td>
+				<td>$ {{ number_format($payment->moratorium, 2) }}</td>
 				<td><p>{{$payment->status}}</p></td>
 			</tr>
 			@elseif($payment->status == "Vencido")
 			<tr class="danger">
 				<td>{{ $payment->number }}</td>
 				<td>{{$payment->date->format('d F Y')}}</td>
-				<td> <button  type="button" class="btn btn-danger btn-lg" id="vencido" {{-- onclick="myFunction()" data-toggle="modal" data-target="#payment_{{ $payment->id }}" --}}>$ {{ number_format($payment->total, 2) }}</button></td>
-				<td>$ {{ number_format($payment->ammount, 2) }}</td>
-				<td>$ {{ number_format($payment->moratorium, 2) }}</td>
+				<td>${{number_format($payment->total,2)}}</td>
 				<td>${{ number_format($payment->payment, 2)}}</td>
-				<td>${{ number_format($payment->balance, 2) }}</td>
+				<td>$ {{ number_format($payment->balance, 2) }}</td>
+				<td>$ {{ number_format($payment->capital, 2) }}</td>
+				<td>$ {{ number_format($payment->interest, 2) }}</td>
+				<td>$ {{ number_format($payment->moratorium, 2) }}</td>
 				<td><p>{{$payment->status}}</p></td>
 			</tr>
 			@elseif($payment->status == "Pagado")
 			<tr class="success">
 				<td>{{ $payment->number }}</td>
 				<td>{{$payment->date->format('d F Y')}}</td>
-				<td> <button type="button" class="btn bg-default btn-lg disabled">$ {{ number_format($payment->total, 2) }}</button></td>
-				<td>$ {{ number_format($payment->ammount, 2) }}</td>
-				<td>$ {{ number_format($payment->moratorium, 2) }}</td>
+				<td>${{number_format($payment->total,2)}}</td>
 				<td>${{ number_format($payment->payment, 2)}}</td>
-				<td>${{ number_format($payment->balance, 2) }}</td>
+				<td>$ {{ number_format($payment->balance, 2) }}</td>
+				<td>$ {{ number_format($payment->capital, 2) }}</td>
+				<td>$ {{ number_format($payment->interest, 2) }}</td>
+				<td>$ {{ number_format($payment->moratorium, 2) }}</td>
 				<td><p>{{$payment->status}}</p></td>
 			</tr>
 			@endif
@@ -84,4 +82,3 @@
 			alert("¿ESTAS SEGURO DE REALIZAR ESTE PAGO?");
 		}
 </script>
-

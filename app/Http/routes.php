fd86d63186dae482f9c3b3f1d0f10c379b560ef3
WILFRIDORAMIREZ <wilirm23@gmail.com>
2017-12-07 09:42:48 -0600

    se creo ruta

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -995,47 +995,20 @@ Route::get('applyMoratoriumDate',function(){
         $payments = $debt->payments;
 
         foreach ($payments as $key => $payment) {
-            if ($payment->date = $date_now && $payment->status == 'Pendiente' && $payment->date = '2017-12-06' ) {
+             if ($payment->date >= $date_now && $payment->status == 'Vencido' ) {
                 $payment = App\Models\Payment::find($payment->id);
-                $payment->status = 'Vencido';
-                $payment->moratorium = 20;
-                $payment->total = $payment->ammount + $payment->moratorium;
-                $payment->balance = $payment->balance + 20;
+                $payment->status = 'Pendiente';
+                $payment->moratorium = 0;
+                $payment->total = $payment->ammount;
+                $payment->balance = $payment->balance - 20;
                 $payment->save();
 
                 $debt = $payment->debt;
-                $debt->ammount = $debt->ammount + 20;
+                $debt->ammount = $debt->ammount - 20;
                 $debt->save();
             }
-            if ($payment->date = $date_now && $payment->status == 'Parcial' && $payment->date = '2017-12-06') {
-                $payment = App\Models\Payment::find($payment->id);
-                $payment->status = 'Vencido';
-                $payment->moratorium = 20;
-                $payment->total = $payment->ammount + $payment->moratorium;
-                $payment->balance = $payment->balance + 20;
-                $payment->save();
-
-                $debt = $payment->debt;
-                $debt->ammount = $debt->ammount + 20;
-                $debt->save();
-            }
-            $latePayments = $payment->latePayments;
-
-            if ($payment->status == "Vencido") {
-                if ($latePayments->count() == 0) {
-                    $latePayments = new App\Models\LatePayments;
-                    $latePayments->late_number = $payment->number;
-                    $latePayments->late_ammount = $payment->total;
-                    $latePayments->late_payment = $payment->payment;
-                    $latePayments->status = "Atrasado";
-                    $latePayments->payment_id = $payment->id;
-                    $latePayments->debt_id    = $debt->id;
-                    $latePayments->branch_id = $debt->branch_id;
-                    $latePayments->region_id = $debt->region_id;
-                    $latePayments->save();
-                }
-
-            }
+           
+      
 
         }
     }

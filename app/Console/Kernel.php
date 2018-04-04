21901dfa8bb8ebe08f0bc85b11b2e29bfa6f1bbe
WILFRIDORAMIREZ <wilirm23@gmail.com>
2018-04-04 16:27:55 -0500

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
     
         //$schedule->command('lock:payments')->dailyAt('20:15');
 
-        $schedule->command('command:cut')->everyMinute();
+        $schedule->command('command:cut')->dailyAt(23:59);
 
 
     }
